77fd460671b30a7bda5db59317171142106ee3f9
adam-m07 <adamg.moore007@gmail.com>
2026-08-05 17:14:18 +0100

    Linking spotify API

--- script.js
+++ script.js
@@ -23,3 +23,57 @@ if (globalTopTenBtn) {
         alert("Showing global top ten albums (placeholder).");
     });
 }
+
+const clientId = "bfa6d10ccfa946508ae22badd7138b8a";
+const redirectUri = "http://127.0.0.1:5500/index.html";
+
+function generateRandomString(length) {
+    const possible =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    let text = "";
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+
+    return text;
+}
+
+async function generateCodeChallenge(verifier) {
+
+    const data = new TextEncoder().encode(verifier);
+
+    const digest = await window.crypto.subtle.digest("SHA-256", data);
+
+    return btoa(String.fromCharCode(...new Uint8Array(digest)))
+        .replace(/\+/g, "-")
+        .replace(/\//g, "_")
+        .replace(/=/g, "");
+}
+
+document.getElementById("login").onclick = async () => {
+
+    const verifier = generateRandomString(64);
+    const challenge = await generateCodeChallenge(verifier);
+
+    localStorage.setItem("verifier", verifier);
+
+    const params = new URLSearchParams({
+        response_type: "code",
+        client_id: clientId,
+        scope: "user-read-private user-read-email",
+        redirect_uri: redirectUri,
+        code_challenge_method: "S256",
+        code_challenge: challenge
+    });
+
+    window.location =
+        "https://accounts.spotify.com/authorize?" + params.toString();
+};
+
+const params = new URLSearchParams(window.location.search);
+
+const code = params.get("code");
+
+console.log(code);
